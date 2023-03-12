faa45e055039bd1a411e8d5a655dcbb8f94fe14e
Saïd Benaissa <said@vivaldi.com>
2023-03-12 23:54:09 +0100

    avoid using copy constr, copy assigOp, move-constr, move-assig-op

--- main.cpp
+++ main.cpp
@@ -3,17 +3,29 @@
 class Singleton {
 protected:
     Singleton() = default;
+    Singleton(const Singleton &) = delete; // Copy constructor This will prevent creating more than one object of Singleton class
+                                            // not include this function on our class
+
+    Singleton &operator=(const Singleton &) = delete; // Copy assignment operator This will prevent creating more than one object of Singleton class
+                                            // not include this function on our class
+    Singleton(Singleton &&) = delete; // Move constructor is deleted, // This will prevent creating more than one object of Singleton class
+                                            // not include this function on our class
+    Singleton &operator=(Singleton &&) = delete; // Move assignment operator is deleted, // This will prevent creating more than one object of Singleton class
+                                            // not include this function on our class
 
 public:
     int data;
 
     static Singleton &getInstance() {
-        static Singleton instance;
+        static Singleton instance; // This is a static variable, it will be created only once, thread safe and it will be destroyed when the program ends
+                                    // can be used by all the threads  (static variable) and it will be created only once
         return instance;
     }
 };
 
 int main() {
+    // Singleton singleton = Singleton::getInstance(); // This a copy constructor call, we have to make a copy constructor as deleted in order
+                                                    // to prevent creating more that one object of Singleton class
     Singleton &s1 = Singleton::getInstance();
     s1.data = 10;
     std::cout << "s1.data = " << s1.data << std::endl;
